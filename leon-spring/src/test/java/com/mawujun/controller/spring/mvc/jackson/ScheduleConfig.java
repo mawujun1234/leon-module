eed5b4ee7a90ef8218a4c2ae8cd51cfef00cc51c
mawujun1234 <mawujun1234@163.com>
2016-07-22 17:55:18 +0800

    111

--- leon-spring/src/test/java/com/mawujun/controller/spring/mvc/jackson/ScheduleConfig.java
+++ leon-spring/src/test/java/com/mawujun/controller/spring/mvc/jackson/ScheduleConfig.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * http://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/scheduling/annotation/EnableScheduling.html
  * 参考spring的 EnableScheduling 配置信息,里面有多种配置方式
+ * 要实现SchedulingConfigurer接口，不然在启动的时候会爆  类没定义的异常
  * @author mawujun qq:16064988 mawujun1234@163.com
  *
  */
