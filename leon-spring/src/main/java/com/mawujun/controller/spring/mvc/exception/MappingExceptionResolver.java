818dc06c25f3488dd2179540ffa0cfb50861f985
mawujun1234 <mawujun1234@163.com>
2016-07-25 20:35:36 +0800

    12

--- leon-spring/src/main/java/com/mawujun/controller/spring/mvc/exception/MappingExceptionResolver.java
+++ leon-spring/src/main/java/com/mawujun/controller/spring/mvc/exception/MappingExceptionResolver.java
@@ -28,7 +28,7 @@ public class MappingExceptionResolver extends SimpleMappingExceptionResolver {
 	private static Logger logger=LogManager.getLogger(MappingExceptionResolver.class);
 	
 	private String errorMsgAttribute = "errorMsg";//异常消息的属性名称，可以自定义,也就是对异常进行文字化描述，而不是其他信息
-	private String defaultErrorMsg="操作失败，请稍候重试!如果多次操作无效,请联系管理员!";
+	private String defaultErrorMsg="系统异常，请稍候重试!如果多次操作无效,请联系管理员!";
 	/**
 	 * key是viewname，value是错误消息
 	 */
@@ -62,9 +62,10 @@ public class MappingExceptionResolver extends SimpleMappingExceptionResolver {
 			return ((BusinessException)ex).getMessage();
 		} else if (this.errorMsgs.containsKey(ex.getClass())) {
 			return this.errorMsgs.get(ex.getClass());
-		} else if(ex.getMessage()!=null && !"".equals(ex.getMessage())){
-			return ex.getMessage();
 		} 
+//		else if(ex.getMessage()!=null && !"".equals(ex.getMessage())){
+//			return ex.getMessage();
+//		} 
 		//只有在ExceptionMappings没有定义过的时候才会走到这里
 		return this.defaultErrorMsg;
 		
